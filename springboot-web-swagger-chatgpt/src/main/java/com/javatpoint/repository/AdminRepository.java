79a4a7b0c3433579deb52a8bea573a1218355afb
djrabi007 <rabi007007@gmail.com>
2023-07-02 16:56:44 +0530

    Delete springboot-web-swagger-chatgpt directory
    Deleted

--- springboot-web-swagger-chatgpt/src/main/java/com/javatpoint/repository/AdminRepository.java
+++
@@ -1,8 +0,0 @@
-package com.javatpoint.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.javatpoint.model.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, String> {
-}
