59fdde2cc661fcc4ee9579eb7fad48529cd66260
djrabi007 <rabi007007@gmail.com>
2023-07-14 09:28:50 +0530

    Delete springboot-web-swagger-chatgpt directory
    deleted

--- springboot-web-swagger-chatgpt/src/main/java/com/javatpoint/service/ICustomer.java
+++
@@ -1,9 +0,0 @@
-package com.javatpoint.service;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.javatpoint.model.Customer;
-
-public interface ICustomer extends CrudRepository<Customer, Long> {
-
-}
