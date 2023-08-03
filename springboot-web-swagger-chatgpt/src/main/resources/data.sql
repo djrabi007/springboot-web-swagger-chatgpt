4daf548123097af30530b8d59f17100a200791e8
djrabi007 <rabi007007@gmail.com>
2023-08-03 09:21:45 +0530

    Delete springboot-web-swagger-chatgpt directory
    del

--- springboot-web-swagger-chatgpt/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO change_requests (id, cr_number, creation_date_time, created_by, account_group)
-VALUES (1, 'CR-001', '2023-07-15 10:30:00', 'JohnDoe', 'Account Group 1');
-
-INSERT INTO change_requests (id, cr_number, creation_date_time, created_by, account_group)
-VALUES (2, 'CR-002', '2023-07-16 09:45:00', 'JaneSmith', 'Account Group 2');
-
