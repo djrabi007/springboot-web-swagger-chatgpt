31fa63777a474b695a5495b48ec57816a6f3e46e
djrabi007 <rabi007007@gmail.com>
2023-08-16 11:19:55 +0530

    Delete springboot-web-swagger-chatgpt directory
    del

--- springboot-web-swagger-chatgpt/src/main/java/com/javatpoint/dao/CustomerReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.javatpoint.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.javatpoint.model.CustomerReview;
-
-public interface CustomerReviewRepository extends JpaRepository<CustomerReview, Long> {
-	// Add custom queries or methods if needed
-}
