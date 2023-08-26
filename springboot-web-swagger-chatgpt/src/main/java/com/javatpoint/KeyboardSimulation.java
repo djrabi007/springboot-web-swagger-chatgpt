0a5ef5faf97780abdcb1a1b7ff2b6c553e30bea5
djrabi007 <rabi007007@gmail.com>
2023-08-26 19:30:56 +0530

    Delete entire line

--- springboot-web-swagger-chatgpt/src/main/java/com/javatpoint/KeyboardSimulation.java
+++ springboot-web-swagger-chatgpt/src/main/java/com/javatpoint/KeyboardSimulation.java
@@ -9,12 +9,15 @@ public class KeyboardSimulation {
 		while (true) {
 			// Simulate typing a paragraph
 			//String st = "I am writing a Java program for keyboard simulation. This program uses the Robot class to simulate key presses and releases. It can be used for various automation tasks that involve keyboard input. In this example, I am typing a paragraph using Java.";
-			String st = "I like to work in Truist  Bank and open account";
+			// String st = "I like to work in Truist Bank and open account";
+			String st = "The Truist Zelle application embodies the future of digital banking, where convenience, speed, and security are paramount. As consumers increasingly rely on mobile devices for financial management, this innovative solution provides a valuable tool for simplifying monetary transactions and fostering financial inclusion";
 			typeParagraph(robot,
 					st);
 
 			// Delay for 10 seconds before typing again
 			Thread.sleep(10000);
+
+			deleteEntireLine(robot);
 
 
 		}
@@ -49,6 +52,24 @@ public class KeyboardSimulation {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	private static void deleteEntireLine(Robot robot) {
+
+		// Press and hold the Shift key
+		robot.keyPress(KeyEvent.VK_SHIFT);
+
+		// Press the Home key to select from the cursor to the beginning of the line
+		robot.keyPress(KeyEvent.VK_HOME);
+		robot.keyRelease(KeyEvent.VK_HOME);
+
+		// Release the Shift key to select the entire line
+		robot.keyRelease(KeyEvent.VK_SHIFT);
+
+		// Press the Delete key to delete the selected line
+		robot.keyPress(KeyEvent.VK_DELETE);
+		robot.keyRelease(KeyEvent.VK_DELETE);
+
 	}
 
 	private static void ctrl_S_to_Save_File(Robot robot) {
